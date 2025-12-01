3992f4e0e3056649883fb3d17cef2321a4a3165e
lenasticot <lena.odum1@gmail.com>
2025-12-01 18:13:09 +0100

    final changes

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: leodum <leodum@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/11 12:18:25 by leodum            #+#    #+#             */
-/*   Updated: 2025/12/01 16:40:09 by leodum           ###   ########.fr       */
+/*   Updated: 2025/12/01 18:07:15 by leodum           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,5 +27,6 @@ char	*get_next_line(int fd);
 size_t	ft_strlen(const char *s);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strchr(const char *s, int c);
+void	*ft_calloc(size_t count, size_t size);
 
 #endif
